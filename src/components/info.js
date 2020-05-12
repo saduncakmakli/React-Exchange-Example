5e5aff9f0b92db485827034e2d627323ecd8c48e
Muhammed Sadun Çakmaklı <muhammedsaduncakmakli@gmail.com>
2020-05-12 23:11:17 +0300

    basic change

--- src/components/info.js
+++ src/components/info.js
@@ -6,9 +6,9 @@ function InfoComponent(props) {
     return (
       <div
         ref={props.infoDiv}
-        className={"Info"}
+        className={"Info"} //Css için
         style={{ top: numberAddpx(props.positon_top), left: numberAddpx(props.positon_left) }}
-        onMouseDown={(event) => props.handleOnMouseDown(event, "Info")}
+        onMouseDown={(event) => props.handleOnMouseDown(event, "Info")} //HTML Event
       >
         <p style={{ font: '19px "Comic Sans MS"', color: 'red', textAlign: 'center' }}>All components in this project can be moved with the mouse in the component it is in. <br /> The last click is always on top.</p>
   
